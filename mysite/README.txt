12888048ed3f3665f67560fa1d5b99d5d8874c28
Jimmy ThinkPad <jimmida@gmail.com>
2013-03-30 19:51:49 -0700

    django tutorials

---
+++ mysite/README.txt
@@ -0,0 +1,10 @@
+Learning points:
+1. Set up database, timezone, template_dir
+2. Create models, model functions, syncdb
+3. Write views
+  a) include context
+  b) return HttpResponse objects
+  c) get_object_or_404
+4. Edit urls, pass in parameter
+5. Using generic views
+6. Create templates
---
+++ mysite/polls/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+class Poll(models.Model):
+  question = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
+
+  def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date < now
+
+  was_published_recently.admin_order_field = 'pub_date'
+  was_published_recently.boolean = True
+  was_published_recently.short_description = 'Published recently?'
+
+  # sets the default return
+  def __unicode__(self):
+    return self.question
+
+class Choice(models.Model):
+  poll = models.ForeignKey(Poll)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
+
+  # sets the default return
+  def __unicode__(self):
+    return self.choice_text
